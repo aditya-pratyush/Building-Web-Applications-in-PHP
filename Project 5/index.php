9710752df609affc2a063bc0b73e173bd33325f9
Aditya Pratyush <aditya.katihar19@gmail.com>
2020-07-22 23:56:21 +0530

    Web Applications in PHP

---
+++ Project 5/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Ivan Neradovic PHP</title>
+	</head>
+	<body>
+		<h1>Aditya Pratyush PHP</h1>
+
+		<pre>ASCII ART (First letter of the Name 'Aditya' - 'A'):
+
+    	****
+     **		 **
+     **		 **
+     **********
+     **		 **
+     **		 **
+    
+		</pre>
+
+		<p>
+			The SHA256 hash of "Aditya Pratyush" is <?php print hash('sha256', 'Ivan Neradovic'); ?>
+		</p>
+	
+	</body>
+</html>
